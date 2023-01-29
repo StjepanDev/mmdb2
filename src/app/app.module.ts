1c6e5d98216af14794270de3d0a6db1583eb9105
StjepanDev <stjepansuhic@yahoo.com>
2023-01-29 14:33:34 +0100

    banner comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MoviesComponent } from './pages/movies/movies.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SliderComponent } from './components/slider/slider.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ItemsBannerComponent } from './components/items-banner/items-banner.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HomeComponent,
     MoviesComponent,
     SliderComponent,
+    ItemsBannerComponent,
   ],
 
   imports: [
--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,3 +1,10 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, OnInit, Input } from '@angular/core';
 import { Movie } from '../../models/movie';
 
@@ -5,10 +12,21 @@ import { Movie } from '../../models/movie';
   selector: 'slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss'],
+  animations: [
+    trigger('slideFade', [
+      state('void', style({ opacity: 0 })),
+      transition('void <=> *', [animate('1s')]),
+    ]),
+  ],
 })
 export class SliderComponent implements OnInit {
   @Input() items: Movie[] = [];
 
-  constructor() {}
-  ngOnInit(): void {}
+  currentSlideIndex: number = 0;
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.currentSlideIndex = ++this.currentSlideIndex % this.items.length;
+    }, 3000);
+  }
 }
